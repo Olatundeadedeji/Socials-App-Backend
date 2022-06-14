af919d246b043640464da3b67427f501c383e704
Olatunde Adedeji <olatundeadedeji32@gmail.com>
2022-06-14 21:16:55 +0100

    updated index.js

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO_DB)
   .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    )
+    app
+      .listen(process.env.PORT, () =>
+        console.log(`Server running on port ${process.env.PORT}`)
+      )
+      .catch((error) => console.log(error))
   );
 //Set up routes
