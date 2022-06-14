cc58ae224998631eb5a1a42c7c3fd4806377d4a6
Olatunde Adedeji <olatundeadedeji32@gmail.com>
2022-06-14 21:45:27 +0100

    updated authentication controller

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -15,5 +15,5 @@ try {
   await newUser.save();
   res.status(201).json(newUser);
 } catch (error) {
-  res.status(500).json("message:error.message");
+  res.status(500).json({ message: error.message });
 }
