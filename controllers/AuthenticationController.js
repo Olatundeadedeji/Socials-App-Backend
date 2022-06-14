500cea21d7a469c7459ca5bf52d682452194f35b
Olatunde Adedeji <olatundeadedeji32@gmail.com>
2022-06-14 21:30:37 +0100

    added authenticationController

---
+++ controllers/AuthenticationController.js
@@ -0,0 +1,19 @@
+import UserModel from "@models/UserModel";
+
+export const userRegister = async (req, res) => {
+  const { username, email, password, firstname, lastname } = req.body;
+  const newUser = new UserModel({
+    username,
+    email,
+    password,
+    firstname,
+    lastname,
+  });
+};
+
+try {
+  await newUser.save();
+  res.status(201).json(newUser);
+} catch (error) {
+  res.status(500).json("message:error.message");
+}
