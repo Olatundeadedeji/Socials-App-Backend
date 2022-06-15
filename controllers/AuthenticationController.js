3a47ba53fcf3b1d4a3874543b5bfa91289c59d40
Olatunde Adedeji <olatundeadedeji32@gmail.com>
2022-06-15 23:31:19 +0100

    hashedpwd working

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -1,11 +1,18 @@
 import UserModel from "../models/UserModel.js";
+import bcrypt from "bcrypt";
 
+//Register a user
 export const userRegister = async (req, res) => {
   const { username, email, password, firstname, lastname } = req.body;
+
+  //Setup bcrypt salt
+  const saltKey = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, saltKey);
+
   const newUser = new UserModel({
     username,
     email,
-    password,
+    password: hashedPassword,
     firstname,
     lastname,
   });
