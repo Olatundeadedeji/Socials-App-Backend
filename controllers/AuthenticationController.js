a8a0215a1bdea8231180b59c87dc9fdb8f09d572
Olatunde Adedeji <olatundeadedeji32@gmail.com>
2022-06-16 01:04:10 +0100

    userLogin added

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -23,3 +23,22 @@ export const userRegister = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+//User login
+
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email: email });
+    if (user) {
+      const isValid = await bcrypt.compare(password, user.password);
+      isValid
+        ? res.status(200).json(user)
+        : res.status(400).json("Password is wrong");
+    } else {
+      res.status(404).json("User not found");
+    }
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
